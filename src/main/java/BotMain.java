ef8061447c582d2bb3f71d73bfa82dd340b3c0fc
fmn0012300 <fmn4@uw.edu>
2020-04-30 14:10:51 -0700

    change token

--- src/main/java/BotMain.java
+++ src/main/java/BotMain.java
@@ -5,6 +5,6 @@ import javax.security.auth.login.LoginException;
 
 public class BotMain {
     public static void main (String[] args) throws LoginException {
-        JDA jda = new JDABuilder("NzA1NDk4NjM4NjkxOTkxNjQy.XqslLg.1erEscmq4zl2Jv14hGNE_xmw3Ik").build();
+        JDA jda = new JDABuilder("token").build();
     }
 }
