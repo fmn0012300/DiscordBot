00c025b8f774464afc25fcd98661d5bdacc6a9da
fmn0012300 <fmn4@uw.edu>
2020-04-30 21:00:41 -0700

    done

--- src/main/java/BotMain.java
+++ src/main/java/BotMain.java
@@ -7,7 +7,7 @@ import javax.security.auth.login.LoginException;
 
 public class BotMain {
     public static void main(String[] args) throws LoginException {
-        JDA jda = new JDABuilder("NzA1NDk4NjM4NjkxOTkxNjQy.XquBgg.uJPVZ4p74nnu1s4lYLqHIkdUzfE").build();
+        JDA jda = new JDABuilder("token").build();
 
         jda.addEventListener(new GuildWarEvents());
     }
